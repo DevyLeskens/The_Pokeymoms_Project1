5f8c30b75f61c90c4d0bb7496085f7ee0a41d313
Devy Leskens <devy.leskens@student.ucll.be>
2021-03-29 15:45:23 +0200

    Story 2, 3, 4 are finished! Great job guys! :)

---
+++ The_Pokeymoms_Project1/src/domain/Cirkel.java
@@ -0,0 +1,46 @@
+package domain;
+
+public class Cirkel {
+    private Punt middelpunt;
+    private int radius;
+
+    public Cirkel(Punt punt, int radius) {
+        setMiddelPunt(punt);
+        setRadius(radius);
+    }
+
+    private void setMiddelPunt(Punt punt) {
+        if (punt == null) {
+            throw new DomainException("Kan niet leeg zijn");
+        }
+        this.middelpunt = punt;
+    }
+
+    private void setRadius(int radius) {
+        if (radius <= 0) {
+            throw new DomainException("radius mag niet kleiner of gelijk aan 0 zijn");
+        }
+        this.radius = radius;
+    }
+
+    public Punt getMiddelpunt() {
+        return this.middelpunt;
+    }
+
+    public int getRadius() {
+        return this.radius;
+    }
+
+    public boolean equals(Object cirkel) {
+        if (cirkel == null) {
+            return false;
+        }
+        return ((Cirkel) cirkel).getRadius() == this.radius && ((Cirkel) cirkel).getMiddelpunt() == this.middelpunt;
+    }
+
+    public String toString() {
+        return ("Cirkel met middelpunt " + middelpunt.getX() + ", " + middelpunt.getY() + " en straal " + getRadius());
+    }
+
+
+}
---
+++ The_Pokeymoms_Project1/tests/domain/CirkelTest.java
@@ -0,0 +1,81 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CirkelTest {
+    private Punt middelpunt;
+    private int radius;
+    private Cirkel cirkel;
+
+    @Before
+    public void setUp() throws Exception{
+        middelpunt = new Punt(5,10);
+        radius = 5;
+        cirkel = new Cirkel(middelpunt, radius);
+    }
+//    ik kan een cirkel aanmaken met een geldig middelpunt en een
+//    geldige straal
+
+
+    @Test
+    public void Cirkel_met_geldig_middelpunt_en_straal(){
+        cirkel = new Cirkel(middelpunt, radius);
+    }
+
+
+//    o ik krijg een exception wanneer ik een cirkel wil aanmaken met
+//    middelpunt = null
+
+    @Test (expected = DomainException.class)
+    public void exception_met_middelpunt_null(){
+        new Cirkel(null, radius);
+    }
+
+//    o ik krijg een exception wanneer ik een cirkel wil aanmaken met
+//    straal < 0
+
+    @Test (expected = DomainException.class)
+    public void exception_met_straal_kleiner_0(){
+        new Cirkel(middelpunt, -5);
+    }
+
+
+//    o ik krijg een exception wanneer ik een cirkel wil aanmaken met
+//    straal = 0
+
+    @Test (expected = DomainException.class)
+    public void Gooit_exception_als_straal_0_is(){
+        new Cirkel(middelpunt, 0);
+    }
+
+//    o Twee cirkels zijn gelijk wanneer ze hetzelfde middelpunt en
+//    dezelfde straal hebben
+
+    @Test
+    public void equals_zelfde_geg(){
+        assert(cirkel.equals(new Cirkel(middelpunt, radius)));
+    }
+
+//    o Twee cirkels zijn verschillend wanneer de tweede cirkel null is
+    @Test
+    public void equals_niet_dezelfde_cirkel(){
+        assert(!cirkel.equals(null));
+    }
+//    o Twee cirkels zijn verschillend wanneer hun middelpunt verschillend
+//            is
+    @Test
+    public void equals_verschillend_mid(){
+        Cirkel newCirkel = new Cirkel(new Punt(10,5), radius);
+        assertFalse(cirkel.equals(newCirkel));
+    }
+
+//    o Twee cirkels zijn verschillend wanneer hun straal verschillend is
+    @Test
+    public void Verschillende_stralen(){
+        assert(!cirkel.equals(new Cirkel(middelpunt, 2)));
+    }
+}
